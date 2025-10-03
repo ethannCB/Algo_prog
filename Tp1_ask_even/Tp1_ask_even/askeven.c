e12a8d3f88d5b40ac62e43616e46715a6f63e169
ethannCB <ethann.campionbaux@gmail.com>
2025-10-03 16:47:01 +0200

    Add files via upload

--- Tp1_ask_even/Tp1_ask_even/askeven.c
+++ Tp1_ask_even/Tp1_ask_even/askeven.c
@@ -2,5 +2,28 @@
 #include "askeven.h"
 
 void askEven() {
-	printf("maybe");
+	int nb, go = 0;
+	do {
+		printf("Entrez un nb svp(0 pour arreter): ");
+		int r = scanf_s("%d", &nb);
+		if (r != 1) {
+			printf("Erreur de saisie\n");
+			scanf_s("%*[^\n]");
+			nb = -1;
+		}
+		if (0 == nb) {
+			go = 0;
+		}
+		else {
+			go = 1;
+		}
+		if (nb > 0) {
+			if (isEven(nb)) {
+				printf("Le nombre %d est pair\n", nb);
+			}
+			else {
+				printf("Le nombre %d est impair\n", nb);
+			}
+		}
+	} while (go);
 }
---
+++ Tp1_ask_even/Tp1_ask_even/iseven.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "iseven.h"
+
+void isEven(int a){
+	if (a % 2 == 0)
+		return 1;
+return 0;
+}
